865e9f636d8fb46f904a0a3e0b4df8035864820d
panindraprakash <panindraprakash@gmail.com>
2018-01-12 20:40:56 +0530

    Delete update

--- Implementation Of A Singly Linked List.cpp
+++ Implementation Of A Singly Linked List.cpp
@@ -42,3 +42,58 @@ struct node
       temp=temp->next;
     }
   } 
+	 void insert_start(int value)
+  {
+    node *temp=new node;
+    temp->data=value;
+    temp->next=head;
+    head=temp;
+  }
+	 void insert_position(int pos, int value)
+  {
+    node *pre=new node;
+    node *cur=new node;
+    node *temp=new node;
+    cur=head;
+    for(int i=1;i<pos;i++)
+    {
+      pre=cur;
+      cur=cur->next;
+    }
+    temp->data=value;
+    pre->next=temp;	
+    temp->next=cur;
+  }
+	  void delete_first()
+  {
+    node *temp=new node;
+    temp=head;
+    head=head->next;
+    delete temp;
+  }
+	   void delete_last()
+  {
+    node *current=new node;
+    node *previous=new node;
+    current=head;
+    while(current->next!=NULL)
+    {
+      previous=current;
+      current=current->next;	
+    }
+    tail=previous;
+    previous->next=NULL;
+    delete current;
+  }
+	     void delete_position(int pos)
+  {
+    node *current=new node;
+    node *previous=new node;
+    current=head;
+    for(int i=1;i<pos;i++)
+    {
+      previous=current;
+      current=current->next;
+    }
+    previous->next=current->next;
+  }
